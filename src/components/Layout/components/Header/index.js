4ca647327ae965776c1ed1c6279c3b6423de77b5
truong <nntruongit2404@gmail.com>
2023-01-02 00:01:29 +0700

    contruct layout basic tt

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,17 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h1>Header</h1>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* logo */}
+                {/* search */}
+            </div>
+        </header>
+    );
 }
 
 export default Header;
